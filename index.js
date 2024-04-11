20667b3a08f276e00392a688769f3252dbcde5e5
Hunter Wyatt <hunterwyatt@C02F10G8MD6R.local>
2024-04-11 11:46:45 -0600

    4/11/24

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const menuIcon = document.getElementById("openIcon");
 const closeIcon = document.getElementById("closeIcon");
 const navLinks = document.getElementById("navLinks");
 
+const deploymentID = 'AKfycbz1YlatEtWIqKkgelgH0C9SqJNv-_zdb2J__-nUY1vQxkNJA88ZQYp3JBvvJsXxjkSw';
+
 menuBtn.addEventListener("click", () => {
   navLinks.classList.toggle("hidden");
   navLinks.classList.add("flex");
@@ -16,8 +18,6 @@ menuBtn.addEventListener("click", () => {
   closeIcon.classList.toggle("hidden");
 });
 
-// document.getElementById("submit-order").addEventListener("click", postData);
-
 function postOrder(args) {
   const overlay = document.getElementById("loading-overlay");
   const button = document.getElementById("order-button");
@@ -28,7 +28,7 @@ function postOrder(args) {
   const failure = document.getElementById("order-failure");
 
   fetch(
-    "https://script.google.com/macros/s/AKfycbz5LiLBdNazIAzcdfv8YNgE_iNzZdiH2zVlllFycbYbxYL0DjTyKz5dg8nVvgfWZwhF/exec",
+    `https://script.google.com/macros/s/${deploymentID}/exec`,
     {
       method: "POST",
       redirect: "follow",
@@ -46,7 +46,7 @@ function postOrder(args) {
     })
     .then((data) => {
       overlay.classList.remove("flex");
-      button.disabled = false;
+      // button.disabled = false;
       success.style.display = "block";
       success.scrollIntoView({ behavior: "smooth", block: "center" });
     })
@@ -69,6 +69,7 @@ document.addEventListener("DOMContentLoaded", function () {
       const coffee = document.getElementById("coffee").value;
       const name = document.getElementById("name").value.trim();
       const address = document.getElementById("address").value.trim();
+      const email = document.getElementById("email").value.trim();
       const phoneNumber = document.getElementById("phone").value.trim();
       const weight = document.querySelector(
         'input[name="bordered-radio"]:checked'
@@ -89,6 +90,11 @@ document.addEventListener("DOMContentLoaded", function () {
         return;
       }
 
+      if (!email) {
+        alert("Please enter your email address.");
+        return;
+      }
+
       if (!phoneNumber) {
         alert("Please enter your phone number.");
         return;
@@ -99,7 +105,9 @@ document.addEventListener("DOMContentLoaded", function () {
         return;
       }
 
-      args = { name, phoneNumber, address, coffee, weight: weight.value };
+      const type = "order"
+
+      args = { name, phoneNumber, email, address, coffee, weight: weight.value, type };
 
       postOrder(args);
     });
@@ -131,45 +139,43 @@ document.addEventListener("DOMContentLoaded", function () {
 
 function sendMessage(args) {
   const overlay = document.getElementById("loading-overlay");
-  const button = document.getElementById("order-button");
+  const button = document.getElementById("contact-button");
   overlay.classList.add("flex");
   button.disabled = true;
 
-  const success = document.getElementById("order-success");
-  const failure = document.getElementById("order-failure");
+  const success = document.getElementById("contact-success");
+  const failure = document.getElementById("contact-failure");
 
-  success.style.display = "block";
-
-  // fetch(
-  //   "https://script.google.com/macros/s/AKfycbz5LiLBdNazIAzcdfv8YNgE_iNzZdiH2zVlllFycbYbxYL0DjTyKz5dg8nVvgfWZwhF/exec",
-  //   {
-  //     method: "POST",
-  //     redirect: "follow",
-  //     headers: {
-  //       "Content-Type": "text/plain;charset=utf-8",
-  //     },
-  //     body: JSON.stringify(args),
-  //   }
-  // )
-  //   .then((response) => {
-  //     if (!response.ok) {
-  //       throw new Error("Network response was not ok");
-  //     }
-  //     return response.text();
-  //   })
-  //   .then((data) => {
-  //     overlay.classList.remove("flex");
-  //     button.disabled = false;
-  //     success.style.display = "block";
-  //     success.scrollIntoView({ behavior: "smooth", block: "center" });
-  //   })
-  //   .catch((error) => {
-  //     overlay.classList.remove("flex");
-  //     button.disabled = false;
-  //     failure.style.display = "block";
-  //     failure.scrollIntoView({ behavior: "smooth", block: "center" });
-  //     console.error("Error:", error);
-  //   });
+  fetch(
+    `https://script.google.com/macros/s/${deploymentID}/exec`,
+    {
+      method: "POST",
+      redirect: "follow",
+      headers: {
+        "Content-Type": "text/plain;charset=utf-8",
+      },
+      body: JSON.stringify(args),
+    }
+  )
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error("Network response was not ok");
+      }
+      return response.text();
+    })
+    .then((data) => {
+      overlay.classList.remove("flex");
+      // button.disabled = false;
+      success.style.display = "block";
+      success.scrollIntoView({ behavior: "smooth", block: "center" });
+    })
+    .catch((error) => {
+      overlay.classList.remove("flex");
+      button.disabled = false;
+      failure.style.display = "block";
+      failure.scrollIntoView({ behavior: "smooth", block: "center" });
+      console.error("Error:", error);
+    });
 }
 
 document.addEventListener("DOMContentLoaded", function () {
@@ -180,7 +186,7 @@ document.addEventListener("DOMContentLoaded", function () {
       event.preventDefault();
 
       const name = document.getElementById("contact-name").value.trim();
-      const address = document.getElementById("contact-address").value.trim();
+      const email = document.getElementById("contact-email").value.trim();
       const phoneNumber = document.getElementById("contact-phone").value.trim();
       const message = document.getElementById("contact-message").value.trim();
 
@@ -189,8 +195,8 @@ document.addEventListener("DOMContentLoaded", function () {
         return;
       }
 
-      if (!address) {
-        alert("Please enter your address.");
+      if (!email) {
+        alert("Please enter your email address.");
         return;
       }
 
@@ -204,7 +210,9 @@ document.addEventListener("DOMContentLoaded", function () {
         return;
       }
 
-      args = { name, phoneNumber, address, message };
+      const type = "contact"
+
+      args = { name, phoneNumber, email, message, type };
 
       sendMessage(args);
     });
